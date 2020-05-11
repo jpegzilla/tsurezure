9edc92957730bc11b31b5bff337e6942e29990d5
jpegzilla <eris@jpegzilla.com>
2020-05-11 02:12:41 -0400

    added some new error codes

--- lib/utils/error_codes.rb
+++ lib/utils/error_codes.rb
@@ -6,7 +6,19 @@ module ErrorCodes
     "invalid parameter: #{item} must be a number."
   end
 
+  def self.no_method_error(item)
+    "invalid http method: #{item} is not a valid http method."
+  end
+
+  def self.invalid_type_error(item)
+    "invalid type: #{item} is not a valid type."
+  end
+
   def self.range_error(min, max)
     "invalid port number: port must be in range [#{min}, #{max}]"
+  end
+
+  def self.invalid_structure_error(keys, method)
+    "invalid object with keys #{keys} supplied to #{method}"
   end
 end
