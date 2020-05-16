266c1cefa3bea2bc0cfa4ca7602ea2fd334aa791
jpegzilla <eris@jpegzilla.com>
2020-05-15 22:43:42 -0400

    the syntax in main continues to get sexier

--- main.rb
+++ main.rb
@@ -4,21 +4,13 @@ require_relative 'lib/tsurezure'
 
 server = Tsurezure.new(8888)
 
-get_root = proc do |_req = nil, _res = nil|
-  'hello world'
-end
+server.register 'get', '/', proc { |req|
+  pp req
+  { status: 200, message: { hello: 'world' }.to_json }
+}, content_type: 'application/json'
 
-get_hello = proc do |_req = nil, _res = nil|
-  'hello world'
-end
-
-# current registration options:
-# imply_get for non-strict request methods,
-# meaning that if you register an endpoint with x method,
-# and someone accesses it by y method, you can allow that
-# 'invalid' request to be treated as a get request
-
-server.register 'get', '/', get_root
-server.register 'get', '/hello', get_hello
+server.register 'get', '/hello', proc { |_req = nil|
+  { status: 404, message: 'hello stinky' }
+}, content_type: 'text/plain'
 
 server.listen
