9efbf4a899c81d9154e7ea4efd13f05415cbd987
jpegzilla <eris@jpegzilla.com>
2020-07-06 13:52:02 -0400

    added more example / test code to main.rb

--- main.rb
+++ main.rb
@@ -4,6 +4,8 @@ require_relative 'lib/tsurezure'
 
 server = Tsurezure.new 8888
 
+# ===================================================================
+
 # server.add_middleware '/', proc { |req|
 #   # puts
 #   # pp 'hello from middleware one! request:'
@@ -17,9 +19,9 @@ server.add_middleware '*', lambda { |req|
 }, {}
 
 server.add_middleware '/', lambda { |req|
-  puts
-  pp 'hello from middleware one! request:'
-  puts req
+  # puts
+  # pp 'hello from middleware one! request:'
+  # puts req
 }, {}
 
 server.add_middleware '/user/:id/articles/:num', lambda { |req|
@@ -34,12 +36,23 @@ server.add_middleware '/e', lambda { |req|
   # puts req
 }, {}
 
-server.register 'get', '/', lambda { |_req = nil|
+server.add_middleware '/user/:id', lambda { |req|
+  # pp req
+  url_vars = req[:vars]
+
+  if req[:vars]['id'] == '1'
+    return { status: 200, message: { message:
+      "hey user ##{url_vars['id']}! you're the first one here!" }.to_json }
+  end
+}, content_type: 'application/json'
+
+# ===================================================================
+
+server.register 'get', '/', lambda { |_req|
   { status: 200, message: { hello: 'world' }.to_json }
 }, content_type: 'application/json'
 
-server.register 'get', '/hello', lambda { |req|
-  pp req
+server.register 'get', '/hello', lambda { |_req|
   { status: 404, message: 'hello stinky' }
 }, content_type: 'text/plain'
 
@@ -48,10 +61,19 @@ server.register 'get', '/e', lambda { |_req = nil|
 }, content_type: 'application/json'
 
 server.register 'get', '/user/:id/articles/:num', lambda { |req|
-  vars = req[:vars]
+  url_vars = req[:vars]
+  params = req[:params]
 
-  { status: 404, message: { message:
-    "hello user ##{vars['id']}, your article is ##{vars['num']}!" }.to_json }
+  { status: 200, message: { message:
+    "hello user ##{url_vars['id']}, your article is ##{url_vars['num']}! you #{params['girl'] == 'true' ? 'are' : 'are not'} a girl." }.to_json }
+}, content_type: 'application/json'
+
+server.register 'get', '/user/:id', lambda { |req|
+  url_vars = req[:vars] # { "id" => "1" }
+  params = req[:params] # {}
+
+  { status: 200, message: { message:
+    "hello user ##{url_vars['id']}!" }.to_json }
 }, content_type: 'application/json'
 
 server.listen
